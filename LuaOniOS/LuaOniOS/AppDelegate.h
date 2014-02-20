34a518cf136ff958002e13489432937c9c254fb7
Ogan Topkaya <otopkaya@me.com>
2014-02-20 14:51:26 +0200

    Project Creation

---
+++ LuaOniOS/LuaOniOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LuaOniOS
+//
+//  Created by Ogan Topkaya on 20/02/14.
+//  Copyright (c) 2014 Peak Games. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
